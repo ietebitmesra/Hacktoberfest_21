d3c26c1de3d76b40b0ace8759dea0e31ab0136ba
DarkSoul26 <sanchitkalsi@gmail.com>
2021-10-10 01:14:52 +0530

    Resolved carousel issue

--- movies.js
+++ movies.js
@@ -111,6 +111,22 @@ formMovies.addEventListener('submit', async(e) => {
 
 // TO GET SIMILAR MOVIES
 var similar_movie_object = {}
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
 
 const getRecom = async(id)=>{
     const res = await axios.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}`)
