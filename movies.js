c9d4591b7fe2629e20b806aa0ef6e5492ec892de
Hritik Chaudhary <48386941+hrit-ik@users.noreply.github.com>
2021-10-11 13:47:55 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -77,7 +77,7 @@ formMovies.addEventListener('submit', async(e) => {
      cast.innerText = cast_names;
      const yt_trailer = document.createElement('a');
      yt_trailer.innerHTML = `<i class="fas fa-play"></i><span style="margin-left: 10px"><b>Watch Trailer</b></span>`;
-     yt_trailer.href = await get_trailer(movie_id);
+     yt_trailer.href = await get_trailer(movie_id); 
      yt_trailer.style.color = '#d6d6d6';
 
     // STYLE CREATED ELEMENTS HERE
@@ -176,6 +176,7 @@ const make_recommendations = async(id)=>{
 
 }
 
+//Gets link for movie trailer
 const get_trailer = async(id)=>{
     const res = await axios.get(`http://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
     const key = res.data.results[0].key
