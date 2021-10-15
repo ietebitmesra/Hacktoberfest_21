d01d1c18747e20a46ce4c76f1b5a92209005c7fe
Hritik Chaudhary <hritikplay@gmail.com>
2021-10-15 19:15:59 +0530

    added popular movies on explore page & made skeleton for others

--- js/explore.js
+++ js/explore.js
@@ -1,56 +1,33 @@
 const apiKey = "f985a3ae5df8738bf04a55864c33128c";
-const movie_card = document.querySelectorAll('.movie-card');
-const movie_img = document.querySelectorAll('.movie-img');
-const tv_card = document.querySelectorAll('.tv-card');
-const tv_img = document.querySelectorAll('.tv-img');
+
     
 window.addEventListener('load',  async (e) => {
     e.preventDefault();
 
     // POPULAR MOVIES
-    const MovieRes = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=` + `${apiKey}` + `&language=en-US&page=1`);
+    const popMovieRes = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=` + `${apiKey}` + `&language=en-US&with_original_language=en&page=1`);
+    const trendMovieRes = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`)
+    const upcomingMovieRes = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
+    const topRatedMovieRes = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+    const freeToWatchMovieRes = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&region=IN&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&watch_region=IN&with_watch_monetization_types=free`)
 
-    // POPULAR TV SHOWS
-    const TvRes = await axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=` + `${apiKey}` + `&language=en-US&page=1`);
-
-    // MOVIE CARDS
-    for (let i = 0; i < 4; i++) {
-        var div = document.createElement('DIV');
-        div.classList.add('card-body');
-
-        var h5 = document.createElement('H5');
-        h5.classList.add('card-title');
-
-        h5.append(MovieRes.data.results[i].original_title);
-        h5.style.textAlign = "center";
-        h5.style.fontSize = "1.15rem";
-        div.append(h5);
-        
-        let hd='w400';
-        var poster = `https://image.tmdb.org/t/p/${hd}/${MovieRes.data.results[i].poster_path}`;
-        movie_img[i].setAttribute('src', poster);
-
-        movie_card[i].append(div);
-    }
-
-    // TV SHOWS CARDS
-    for (let i = 0; i < 4; i++) {
-       
-        var div = document.createElement('DIV');
-        div.classList.add('card-body');
-
-        var h5 = document.createElement('H5');
-        h5.style.textAlign = "center";
-        h5.style.fontSize = "1.15rem";
-        h5.classList.add('card-title');
-
-        h5.append(TvRes.data.results[i].name);
-        div.append(h5);
-        
-        let hd='w400';
-        var poster = `https://image.tmdb.org/t/p/${hd}/${TvRes.data.results[i].poster_path}`;
-        tv_img[i].setAttribute('src', poster);
-
-        tv_card[i].append(div);
-    }
+    const popMovieResult = popMovieRes.data.results
+    popMovieResult.forEach(item => {
+        divAppender('popular', item.poster_path, item.original_title)
+    })
 });
+
+
+const divAppender = (container_class, img_src_path, title)=>{
+    const pop_div = document.createElement("div");
+    const pop_img = document.createElement("img");
+    const pop_name = document.createElement("h6");
+    pop_div.append(pop_img);
+    pop_div.append(pop_name);
+    pop_img.height = '200px';
+    pop_img.src = 'https://image.tmdb.org/t/p/w500/'+img_src_path;
+    pop_name.innerText = title;
+
+    const container = document.querySelector(`.${container_class}`)
+    container.append(pop_div);
+}
